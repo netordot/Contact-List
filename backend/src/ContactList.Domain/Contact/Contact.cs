f35a0932c185ba47085a7d7cb8e339b62742be6d
netordot <sdenisdv1@gmail.com>
2024-10-24 11:20:02 +0300

    PhoneNumber value object added
    regex for phone number works with russian format

--- backend/src/ContactList.Domain/Contact/Contact.cs
+++ backend/src/ContactList.Domain/Contact/Contact.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using ContactList.Domain.Contact.ValueObjects;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,25 @@ using System.Threading.Tasks;
 
 namespace ContactList.Domain.Contact
 {
-    internal class Contact
+    public class Contact
     {
+        public string Name { get; private set; }
+        public PhoneNumber PhoneNumber { get; private set; }
+        public string Description { get; private set; }
+        public Guid Id { get; private set; }
+
+        private Contact(string name, PhoneNumber phoneNumber, string description, Guid id)
+        {
+            Name = name;
+            PhoneNumber = phoneNumber;
+            Description = description;
+            Id = id;
+        }
+
+        public static Contact Create(string name, PhoneNumber phoneNumber, string description, Guid id)
+        {
+            var contact = new Contact(name, phoneNumber, description, id);
+            return contact;
+        }
     }
 }
---
+++ backend/src/ContactList.Domain/Contact/ValueObjects/PhoneNumber.cs
@@ -0,0 +1,37 @@
+﻿using ContactList.Domain.Contact.Shared;
+using CSharpFunctionalExtensions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace ContactList.Domain.Contact.ValueObjects
+{
+    public record PhoneNumber
+    {
+        private const string Pattern = "^((\\+7|8)[\\- ]?)?(\\(?\\d{3}\\)?[\\- ]?)?[\\d\\- ]{7,10}$";
+        public string Number { get; }
+
+        private PhoneNumber(string number)
+        {
+            Number = number;
+        }
+
+        public static Result<PhoneNumber, Error> Create(string number)
+        {
+            if (String.IsNullOrWhiteSpace(number))
+            {
+                return Errors.General.ValueIsRequired("PhoneNumber");
+            }
+            if (!Regex.IsMatch(number, Pattern))
+            {
+                return Errors.General.ValueIsInvalid(number);
+            }
+
+            return new PhoneNumber(number);
+        }
+
+    }
+}
